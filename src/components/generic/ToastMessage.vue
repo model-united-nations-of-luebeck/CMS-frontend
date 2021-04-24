de4db16dc46f7fd57c0da834554be28bcdaedf2b
Tom Rix <tom.rix@web.de>
2021-04-24 22:31:41 +0200

    Refactored snackbars into ToastMessage component

---
+++ src/components/generic/ToastMessage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <v-snackbar v-model="snackbar" :color="color" :timeout="timeout">
+      <v-icon color="white">{{ prepend_icon }}</v-icon>
+      {{ message }}
+      <v-spacer></v-spacer>
+      <template v-slot:action="{ attrs }">
+        <v-btn text v-bind="attrs" @click="snackbar = false">
+          {{ ok_option }}
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToastMessage",
+  props: {
+    message: String,
+    color: String,
+    ok_option: {
+      type: String,
+      default: "OK",
+    },
+    timeout: {
+      type: Number,
+      default: 2000,
+    },
+    prepend_icon: String,
+  },
+  data: () => ({
+    snackbar: null,
+  }),
+
+  methods: {
+    show() {
+      this.snackbar = true;
+    },
+  },
+};
+</script>
