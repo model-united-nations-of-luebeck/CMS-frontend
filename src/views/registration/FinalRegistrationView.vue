13686faee30152ac55b2b20a8f3ed197c5a98e07
Tom Rix <tom.rix@web.de>
2025-09-05 21:11:21 +0200

    Avoid this.$router

--- src/views/registration/FinalRegistrationView.vue
+++ src/views/registration/FinalRegistrationView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 const origin = window.location.origin;
 import { toast } from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
@@ -14,6 +14,7 @@ import { useDisplay } from "vuetify";
 const { mobile } = useDisplay();
 
 const route = useRoute();
+const router = useRouter();
 
 const schoolsStore = useSchoolsStore();
 schoolsStore.getSchool(route.params.school_id);
@@ -159,7 +160,7 @@ const getUniqueMemberOrganizationsFromSchool = () => {
                 }"
                 >{{ origin
                 }}{{
-                  this.$router.resolve({
+                  router.resolve({
                     name: "final-registration-mun-director",
                     params: { mun_director_id: director.id },
                   }).href
@@ -176,7 +177,7 @@ const getUniqueMemberOrganizationsFromSchool = () => {
                 prepend-icon="mdi-content-copy"
                 v-clipboard:copy="
                   `${origin}${
-                    this.$router.resolve({
+                    router.resolve({
                       name: 'final-registration-mun-director',
                       params: { mun_director_id: director.id },
                     }).href
@@ -292,7 +293,7 @@ const getUniqueMemberOrganizationsFromSchool = () => {
                 }"
                 >{{ origin
                 }}{{
-                  this.$router.resolve({
+                  router.resolve({
                     name: "final-registration-delegate",
                     params: { delegate_id: delegate.id },
                   }).href
@@ -305,7 +306,7 @@ const getUniqueMemberOrganizationsFromSchool = () => {
                 prepend-icon="mdi-content-copy"
                 v-clipboard:copy="
                   `${origin}${
-                    this.$router.resolve({
+                    router.resolve({
                       name: 'final-registration-delegate',
                       params: { delegate_id: delegate.id },
                     }).href
