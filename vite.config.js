94bd61050abefa871c9fc498764b7f13babe9ca0
Tom Rix <tom.rix@web.de>
2025-09-21 21:07:29 +0200

    default base should be /

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/trix/cms',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
